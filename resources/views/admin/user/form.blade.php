59353d7a209c3a5c1c6b1e27e589846f5412105c
jay baraiya <jay.baraiya@gnosysdigital.com>
2026-06-26 11:03:39 +0530

    - fix error in user form

--- resources/views/admin/user/form.blade.php
+++ resources/views/admin/user/form.blade.php
@@ -168,7 +168,7 @@
                 <div class="col-md-4">
                     <div class="mb-3">
                         <label class="form-label" for="city-id">City <span class="text-danger">*</span></label>
-                        <input type="text" name="city_id" id="city-id" class="form-control" value={{ old('city_id', $user->city_id) }}>
+                        <input type="text" name="city_id" id="city-id" class="form-control" value={{ old('city_id', $user->city_id ?? '') }}>
                         @error('city_id')
                             <span class="text-danger small">{{ $message }}</span>
                         @enderror
